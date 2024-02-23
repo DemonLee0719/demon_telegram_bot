6327e099fddd8bb5ef1c3022408568e48105da11
DemonLee <libingxue4578@gmail.com>
2024-02-23 17:19:17 +0800

    增加官方是示例，并尝试使用keyboard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -8,7 +8,19 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 
+async def choujiang(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    print('choujiang')
+    keyboard = [
+        [
+            InlineKeyboardButton("Option 1", callback_data="1", url='t.me/sz_wolf'),
+            InlineKeyboardButton("Option 2", callback_data="2"),
+        ],
+        [InlineKeyboardButton("Option 3", callback_data="3")],
+    ]
 
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text("Please choose:", reply_markup=reply_markup)
 
 
 def run():
@@ -16,6 +28,7 @@ def run():
     app = ApplicationBuilder().token("6650155971:AAH7mL4QjBSDgpm1L1R28IZgKevn7l0aLOA").build()
 
     app.add_handler(CommandHandler("hello", hello))
+    app.add_handler(CommandHandler("choujiang", choujiang))
 
     app.run_polling()
 
