95d59e93228571a89edbd26968aaa52457c31d34
DemonLee <libingxue4578@gmail.com>
2024-02-26 20:25:10 +0800

    增加keyboard

---
+++ config.py
@@ -0,0 +1,8 @@
+class Config:
+
+    lottery_dict = {
+        "123456": {
+            "id": "123456",
+            "channel_list": ["t.me/sz_wolf"]
+        }
+    }
--- main.py
+++ main.py
@@ -8,6 +8,14 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    print('start')
+    print(update.message.text)
+    print(update.to_json())
+    await context.bot.send_message(chat_id=update.effective_user.id, text='开始')
+    await update.message.reply_text(f'开始 {update.effective_user.first_name}')
+
+
 async def choujiang(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     print('choujiang')
     keyboard = [
@@ -23,11 +31,22 @@ async def choujiang(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Please choose:", reply_markup=reply_markup)
 
 
+async def start1(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Display a message with a button."""
+    await update.message.reply_html(
+        "This button was clicked <i>0</i> times.",
+        reply_markup=InlineKeyboardMarkup.from_button(
+            InlineKeyboardButton(text="Click me!", callback_data="button")
+        ),
+    )
+
 def run():
     print("run")
     app = ApplicationBuilder().token("6650155971:AAH7mL4QjBSDgpm1L1R28IZgKevn7l0aLOA").build()
 
     app.add_handler(CommandHandler("hello", hello))
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("start1", start1))
     app.add_handler(CommandHandler("choujiang", choujiang))
 
     app.run_polling()
